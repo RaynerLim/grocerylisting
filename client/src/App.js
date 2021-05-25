8f34425bac0a79a2f5b9445365aea2b59140eed8
RaynerLim <RavenNeo530@gmail.com>
2021-05-25 11:49:54 +0800

    direct to login or listing on landing

--- client/src/App.js
+++ client/src/App.js
@@ -54,6 +54,7 @@ function App() {
         
         <div className="container">
           <Switch>
+            <Route exact path ="/" render={props => !isAuthenticated ? (<Login {...props} setAuth={setAuth} />) : (<Redirect to="/listing" />)} />
             <Route exact path="/login" render={props => !isAuthenticated ? (<Login {...props} setAuth={setAuth} />) : (<Redirect to="/listing" />)} />
             <Route exact path="/register" render ={props => !isAuthenticated ? (<Register {...props} setAuth={setAuth}/>) : (<Redirect to="/login" />)} />
             <Route exact path="/listing" render ={props => isAuthenticated ? (<Listing {...props} setAuth={setAuth}/>) : (<Redirect to="/login" />)}/>
